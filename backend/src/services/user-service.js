755d46128138880a8505ad4a41004eec67848efe
Priyabrata Maji <priyabratamaji139@gmail.com>
2023-07-27 02:32:05 +0530

    Added password encription with bcrypt npm

--- backend/src/services/user-service.js
+++ backend/src/services/user-service.js
@@ -2,13 +2,25 @@ const {StatusCodes} = require('http-status-codes');
 
 const { UserRepository } = require('../repositories');
 const AppError = require('../utils/errors/app-error');
-
+const bcrypt = require('bcrypt');
 
 const userRepository = new UserRepository();
 
 async function createUser(data) {
     try {
-        const user = await userRepository.create(data);
+        const plainPassword = data.password;
+
+        let newData;
+        
+        await bcrypt.hash(plainPassword, 9).then(function(hash) {
+            // Store hash in your password DB.
+            console.log(hash);
+            data.password = hash;
+            newData = {'email': data.email, 'password': hash};
+            // console.log(newData, 19);
+        });
+        // console.log(newData, 21);
+        const user = await userRepository.create(newData);
         return user;
     } catch(error) {
         console.log(error);
